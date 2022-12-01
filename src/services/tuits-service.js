0a058128339f21d45af70b5166e60ef5582a63e4
Kartik Tickoo <kartiktickoo@Kartiks-MacBook-Air.local>
2022-11-30 21:23:27 -0500

    a9 Mongo fa22

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
-const TUITS_API = 'http://localhost:4000/api/tuits';
+// const TUITS_API = 'http://localhost:4000/api/tuits';
 
-// const API_BASE = process.env.REACT_APP_API_BASE;
-// const TUITS_API = `${API_BASE}/tuits`;
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
@@ -24,7 +24,7 @@ export const deleteTuit = async (tid) => {
 }
 
 export const updateTuit = async (tuit) => {
-    const response = await axios
+    await axios
         .put(`${TUITS_API}/${tuit._id}`, tuit);
     return tuit
 }
